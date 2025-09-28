e809c2db0a29530d7e2dc9ff6e6837d462187175
Jonathan Pereda <jpered4@wgu.edu>
2025-09-27 18:43:23 -0700

    TTL access and manipulation

--- kv_store.py
+++ kv_store.py
@@ -10,9 +10,12 @@ class KeyValueStore:
         self.record[key] = value
         if lifespan>=0:
             self.ttl[key] = time.time() + lifespan
+        elif key in self.ttl:
+            del self.ttl[key]
+
 
     def get(self, key):
-        if key in self.ttl and self.ttl[key] > time.time():
+        if key in self.ttl and self.ttl[key] < time.time():
             self.delete(key)
         elif key in self.record:
             return self.record[key]
@@ -21,6 +24,39 @@ class KeyValueStore:
     def delete(self, key):
         if key in self.record:
             del self.record[key]
+        if key in self.ttl:
+            del self.ttl[key]
+
+
+    def get_expiration(self,key):
+        _key = self.get(key)
+        if _key and key in self.ttl:
+            return self.ttl[key]
+        return None
+
+    def refresh_time(self,key,lifespan=10):
+        _key = self.get(key)
+        if _key and key in self.ttl:
+            self.ttl[key] = time.time() + lifespan
+            return True
+        return False
+
+    def add_time(self,key,increase=10):
+        _key = self.get(key)
+        if _key and key in self.ttl:
+            self.ttl[key] += increase
+            return True
+        return False
+
+    def decrease_time(self,key,decrease=10):
+        _key = self.get(key)
+        if _key and key in self.ttl:
+            self.ttl[key] -= decrease
+            return True
+        return False
+
+
+
 
 
     
--- test_kv_store.py
+++ test_kv_store.py
@@ -3,14 +3,25 @@ import time
 
 def main():
     store = KeyValueStore()
-    store.add("I cant", "Die")
+    print("Creating with 5s")
     store.add("Hello", "World",5)
     print(store.get("Hello"))
-    print(store.get("I cant"))
+
     print("Waiting 4 seconds")
     time.sleep(4)
+
     print(store.get("Hello"))
-    print(store.get("I cant"))
+
+    print("Adding 3s")
+    store.add_time("Hello", 3)
+
+    print("Waiting 1 seconds")
+    time.sleep(1)
+
+
+    print(store.get("Hello"))
+    
+
 
 if __name__ == "__main__":
     main()
