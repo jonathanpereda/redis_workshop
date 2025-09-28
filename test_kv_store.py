c053f5e70e05581669cebc7bd87dab4c07afd3cd
Jonathan Pereda <jpered4@wgu.edu>
2025-09-27 17:58:56 -0700

    TTL option

--- test_kv_store.py
+++ test_kv_store.py
@@ -1,13 +1,16 @@
 from kv_store import KeyValueStore
-
+import time
 
 def main():
     store = KeyValueStore()
     store.add("I cant", "Die")
-    store.add("Hello", "World",60)
+    store.add("Hello", "World",5)
     print(store.get("Hello"))
     print(store.get("I cant"))
-
+    print("Waiting 4 seconds")
+    time.sleep(4)
+    print(store.get("Hello"))
+    print(store.get("I cant"))
 
 if __name__ == "__main__":
     main()
